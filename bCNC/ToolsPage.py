20753eb469544e05ee32070ff2ebd02e57df686f
Ron Schellberg <1888300+rschell@users.noreply.github.com>
2022-11-18 19:33:19 -0700

    remove non-standard 140-142 parameters

--- bCNC/ToolsPage.py
+++ bCNC/ToolsPage.py
@@ -1199,9 +1199,6 @@ class Controller(_Base):
             ("grbl_130", "float", 200.0, _("$130 X max travel [mm]")),
             ("grbl_131", "float", 200.0, _("$131 Y max travel [mm]")),
             ("grbl_132", "float", 200.0, _("$132 Z max travel [mm]")),
-            ("grbl_140", "float", 200.0, _("$140 X homing pull-off [mm]")),
-            ("grbl_141", "float", 200.0, _("$141 Y homing pull-off [mm]")),
-            ("grbl_142", "float", 200.0, _("$142 Z homing pull-off [mm]")),
         ]
         self.buttons.append("exe")
 
